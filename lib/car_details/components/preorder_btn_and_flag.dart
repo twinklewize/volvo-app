de4884385f9227021168d217924bfee7c13040c6
twinklewize <ivan.stanovoy1337@gmail.com>
2021-10-22 20:26:33 +0300

    preorder_modal_bottom_sheet

--- lib/car_details/components/preorder_btn_and_flag.dart
+++ lib/car_details/components/preorder_btn_and_flag.dart
@@ -19,17 +19,22 @@ class PreOrderBtnAndFlag extends StatelessWidget {
         child: Row(
           children: [
             //pre-order button
-            Container(
-              height: 48,
-              width: size.width - (48 + 36 + 8),
-              decoration: BoxDecoration(
-                color: const Color(0xFF162870),
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: const Center(
-                child: Text(
-                  'Предзаказ',
-                  style: TextStyles.buttonText,
+            GestureDetector(
+              onTap: () {
+                TestDriveModalBottomSheet(context);
+              },
+              child: Container(
+                height: 48,
+                width: size.width - (48 + 36 + 8),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF162870),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: const Center(
+                  child: Text(
+                    'Предзаказ',
+                    style: TextStyles.buttonText,
+                  ),
                 ),
               ),
             ),
@@ -49,6 +54,212 @@ class PreOrderBtnAndFlag extends StatelessWidget {
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Future<dynamic> TestDriveModalBottomSheet(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      backgroundColor: Colors.transparent,
+      builder: (context) {
+        return FractionallySizedBox(
+          heightFactor: MediaQuery.of(context).viewInsets.bottom > 0 ? 1 : 0.92,
+          child: Container(
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+            ),
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(40.0),
+                topRight: Radius.circular(40.0),
+              ),
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 56),
+                    child: SvgPicture.asset(
+                      'assets/icons/volvo_logo.svg',
+                      height: 16,
+                    ),
+                  ),
+                  const SizedBox(height: 11),
+                  const Text(
+                    'VOLVO XC40 Recharge',
+                    style: TextStyle(
+                      color: Color(0xFF555F66),
+                      fontSize: 14,
+                      fontFamily: 'VolvoNovum',
+                    ),
+                  ),
+                  const SizedBox(height: 48),
+                  const Text(
+                    'ЗАПРОС ТЕСТ-ДРАЙВА',
+                    style: TextStyle(
+                      color: Color(0xFF060606),
+                      fontSize: 14,
+                      fontFamily: 'VolvoNovum',
+                    ),
+                  ),
+                  const SizedBox(height: 48),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                      child: const Text(
+                        'Персональные данные',
+                        style: TextStyle(
+                          color: Color(0xFF31383D),
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18,
+                          fontFamily: 'VolvoNovum',
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 18),
+                  CustomTextField(hintText: 'Введите фамилию'),
+                  const SizedBox(height: 10),
+                  CustomTextField(hintText: 'Введите имя'),
+                  const SizedBox(height: 32),
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                      child: const Text(
+                        'Контактные данные',
+                        style: TextStyle(
+                          color: Color(0xFF31383D),
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18,
+                          fontFamily: 'VolvoNovum',
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 18),
+                  CustomTextField(hintText: 'Введите город'),
+                  const SizedBox(height: 10),
+                  CustomTextField(hintText: 'Введите телефон'),
+                  const SizedBox(height: 10),
+                  CustomTextField(hintText: 'Введите почту'),
+                  const SizedBox(height: 40),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      height: 48,
+                      width: size.width - 72,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF162870),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'Отправить запрос',
+                          style: TextStyles.buttonText,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Future<dynamic> CarSeriesModalBottomSheet(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      backgroundColor: Colors.transparent,
+      builder: (context) {
+        return FractionallySizedBox(
+          heightFactor: 0.92,
+          child: Container(
+            padding: EdgeInsets.only(
+              bottom: MediaQuery.of(context).viewInsets.bottom,
+            ),
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(40.0),
+                topRight: Radius.circular(40.0),
+              ),
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 56),
+                    child: SvgPicture.asset(
+                      'assets/icons/volvo_logo.svg',
+                      height: 16,
+                    ),
+                  ),
+                  const SizedBox(height: 11),
+                  const Text(
+                    'XC серия',
+                    style: TextStyle(
+                      color: Color(0xFF555F66),
+                      fontSize: 14,
+                      fontFamily: 'VolvoNovum',
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  const SizedBox(height: 16),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  final String hintText;
+
+  const CustomTextField({
+    Key? key,
+    required this.hintText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      width: size.width - 48,
+      height: 48,
+      decoration: BoxDecoration(
+        color: const Color(0xFFF7F9FA),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: TextField(
+        onChanged: (value) {},
+        decoration: InputDecoration(
+          hintText: hintText,
+          hintStyle: const TextStyle(
+            fontFamily: 'VolvoNovum',
+            fontSize: 16,
+            color: Color(0xFF75777A),
+          ),
+          enabledBorder: InputBorder.none,
+          focusedBorder: InputBorder.none,
         ),
       ),
     );
